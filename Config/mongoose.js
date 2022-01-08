fba61a6f763624f6b3783a38bb4a186cba0efbdd
Gaurav mehra <gauravmehra1298@gmail.com>
2022-01-08 14:01:37 +0530

    heroku

--- Config/mongoose.js
+++ Config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/CSV_upload');
+// mongoose.connect('mongodb://localhost/CSV_upload');
+mongoose.connect('mongodb+srv://mehra213:Zd6UGyhgNDvopnHl@cluster0.pooba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 const db=mongoose.connection;
 
